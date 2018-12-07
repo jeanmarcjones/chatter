913dc9f6734a3e23b5ca52844e8e8fa7cfdcd1d0
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-12-07 10:56:19 +0700

    style: remove eol semicolon

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import '../styles/App.css';
-import * as API from '../utils/api'
+import React, { Component } from 'react'
+import '../styles/App.css'
+import * as PostAPI from '../utils/api_post'
 
 class App extends Component {
 
   componentDidMount() {
     const data = { action: "post", data: "Hello Post!" }
 
-    API
+    PostAPI
       .post(data)
       .then(res => console.log(res))
   }
@@ -17,8 +17,8 @@ class App extends Component {
       <div className="App">
         <p>Hello World!</p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
