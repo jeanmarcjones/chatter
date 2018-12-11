bf14504486524082f4a8338b182ef750cfc7cd49
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-12-11 14:26:24 +0700

    refactor: split api into multiple files

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,13 +3,3 @@ const url = process.env.REACT_APP_READABLE_API_URL || 'http://localhost:3001'
 const headers = {
   Accept: "application/json",
 }
-
-export const post = (data) =>
-  fetch(`${url}/post`, {
-    method: "POST",
-    headers: {
-      ...headers,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  }).then(res => res)
