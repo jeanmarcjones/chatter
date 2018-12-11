4b99ef99b9e52837f039fc0a67a602212cc89bc0
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-12-11 14:43:48 +0700

    feat: use token to identify client across multiple sessions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,14 @@
 export const url = process.env.REACT_APP_READABLE_API_URL || 'http://localhost:3001'
 
+let token = localStorage.token
+
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
 export const headers = {
   Accept: 'application/json'
+}
+
+export const user = {
+  id: token
 }
