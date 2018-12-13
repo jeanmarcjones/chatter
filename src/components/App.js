40564d5c817728f82ce9c3ddf44b069185b605be
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-12-13 15:20:59 +0700

    refactor: add login component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import '../styles/App.css'
 import * as SocketAPI from '../utils/api_socket'
+import Login from '../components/Login'
 
 class App extends Component {
   componentDidMount() {
@@ -11,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hello World!</p>
+        <Login />
       </div>
     )
   }
