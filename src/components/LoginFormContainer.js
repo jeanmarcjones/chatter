58994adae5420cfd9b035e77968b13b1aea2a899
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-12-16 10:03:22 +0700

    feat: route to chat after server authentication

--- src/components/LoginFormContainer.js
+++ src/components/LoginFormContainer.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import * as SocketAPI from '../utils/api_socket'
 import LoginForm from "./LoginForm"
 
 class LoginFormContainer extends Component {
+  state = {
+    toChat: false
+  }
+
   handleFormSubmit = (e) => {
     e.preventDefault()
     SocketAPI
       .join({
         name: this.username.value
-      })
+      }, () => this.setState(() => ({
+        toChat: true
+      })))
   }
 
   render() {
+    if (this.state.toChat === true)
+      return <Redirect to='/' />
+
     return (
       <LoginForm
         handleFormSubmit={this.handleFormSubmit}
