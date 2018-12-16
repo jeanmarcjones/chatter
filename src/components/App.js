5e03329a19576c464f28a64cf966917ce8c415cd
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-12-16 10:04:35 +0700

    feat: added routing switch

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
 import '../styles/App.css'
 import * as SocketAPI from '../utils/api_socket'
 import Login from '../components/Login'
+import Chat from '../components/Chat'
 
 class App extends Component {
   componentDidMount() {
@@ -11,8 +13,19 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Login />
+      <div className='App'>
+        <Switch>
+          <Route
+            exact
+            path='/'
+            render={() => (<Chat />)}
+          />
+          <Route
+            exact
+            path='/login'
+            render={() => (<Login />)}
+          />
+        </Switch>
       </div>
     )
   }
