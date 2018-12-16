5dd979133de54a677ac0a01cb670588ae9cc2368
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2018-12-16 10:02:37 +0700

    refactor: check for server authentication

--- src/utils/api_socket.js
+++ src/utils/api_socket.js
@@ -4,11 +4,13 @@ const socket = openSocket(API.url, {
   reconnection: false
 })
 
-export const join = (data) =>
+export const join = (username, callback) => {
   socket.emit('join', {
     ...API.user,
     name: data.name,
   })
+  socket.on('joined', callback)
+}
 
 export const update = () =>
   socket.on('update', (update) => console.log(update))
