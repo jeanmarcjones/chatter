dfa579c39ac6f4ebbd4a7fb06d50fed5ebe40756
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2019-01-05 09:56:10 +0700

    style: consistent quotes

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import '../styles/LoginForm.css'
+import '../styles/buttons.css'
 
 let LoginForm = ({ handleFormSubmit, username }) => {
   return (
     <form
-      className="form form--login"
+      className='form form--login'
       onSubmit={handleFormSubmit}
     >
-      <input type="text"
+      <input type='text'
              ref={username}
-             autoComplete="off"
+             autoComplete='off'
              required
       />
-      <input type="submit"
-             value="Submit"
+      <input type='submit'
+             value='Submit'
+             className='button'
       />
     </form>
   )
