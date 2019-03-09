7b9a3c521348eead71d8de2d51708aeeecea02df
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2019-03-09 15:10:21 +0700

    feat: connect user to the server

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,8 +1,24 @@
+import openSocket from 'socket.io-client'
+import * as API from '../utils/api'
 import {
   UPDATE_USER
 } from './types'
+
+let socket
 
 export const updateUser = ({ user }) => ({
   type: UPDATE_USER,
   user
 })
+
+export const connectUser = ({ name }) => (dispatch) => {
+  socket = openSocket(API.url)
+
+  socket.emit('join', {
+    ...API.headers,
+    name
+  })
+  socket.on('joined', () => {
+    dispatch(updateUser({ user: { loggedIn: true } }))
+  })
+}
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -2,7 +2,11 @@ import {
   UPDATE_USER
 } from '../actions/types'
 
-export default function user(state, action) {
+const initialState = {
+  loggedIn: false
+}
+
+export default function user(state = initialState, action) {
   const { user } = action
 
   switch (action.type) {
