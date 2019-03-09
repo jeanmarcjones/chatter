099346e9a619814445e1f59d88aff46ccd3ffc38
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2019-03-09 15:56:31 +0700

    refactor: now a stateless functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import '../styles/App.css'
 import Login from '../components/Login'
 import Chat from '../components/Chat'
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Switch>
-          <Route
-            exact
-            path='/'
-            component={Chat}
-          />
-          <Route
-            exact
-            path='/login'
-            component={Login}
-          />
-        </Switch>
-      </div>
-    )
-  }
+let App = () => {
+  return (
+    <div className='App'>
+      <Switch>
+        <Route
+          exact
+          path='/'
+          component={Chat}
+        />
+        <Route
+          exact
+          path='/login'
+          component={Login}
+        />
+      </Switch>
+    </div>
+  )
 }
 
 export default App
