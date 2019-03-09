1f4878bddc4cb3a8325beeb685618012b77416c9
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2019-03-09 15:42:57 +0700

    refactor: remove pre redux code

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import '../styles/App.css'
-import * as SocketAPI from '../utils/api_socket'
 import Login from '../components/Login'
 import Chat from '../components/Chat'
 
 class App extends Component {
-  componentDidMount() {
-    SocketAPI
-      .update()
-  }
-
   render() {
     return (
       <div className='App'>
