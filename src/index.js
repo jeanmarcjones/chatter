5f6dd23853b124323110c2d3b9468a16c2f90a88
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2019-03-25 13:17:11 +0000

    refactor: remove unused code

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import history from './history'
 import reducer from './reducers'
 import './styles/index.css'
 import App from './components/App'
-import * as serviceWorker from './serviceWorker'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
@@ -28,8 +27,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
