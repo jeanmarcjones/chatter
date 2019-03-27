f8605306e6972a2fc84c1253a760ac881c37f19c
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2019-03-27 10:04:32 +0000

    refactor: move store to separate file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
 import { Router } from 'react-router-dom'
 import history from './history'
-import reducer from './reducers'
+import store from './store'
 import './styles/index.css'
 import App from './components/App'
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-const store = createStore(
-  reducer,
-  composeEnhancers(
-    applyMiddleware(logger, thunk)
-  )
-)
 
 ReactDOM.render(
   <Provider store={store}>
